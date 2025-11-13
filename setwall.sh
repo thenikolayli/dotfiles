1432128bcc2190f1f2964e19cc30214188d1ffbf
Nikolay <nikolay.li2008@gmail.com>
2025-11-12 18:25:39 -0800

    now can change wallpapers and update colors

--- setwall.sh
+++ setwall.sh
@@ -1,4 +1,4 @@
-wal --backend colorz -i "$1" -q -s -t -e
+wal --backend colorz -i "$1" -s -t -e
 jq 'walk(if type == "string" then sub("#"; "") end)' \
 ~/.cache/wal/colors.json > ~/.local/share/chezmoi/colors.json
 cp ~/.cache/wal/colors.css ~/.local/share/chezmoi/colors.css
